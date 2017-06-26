29019509fbafa071c83b9d29f815be71aa6e7163
qingqing202 <qingqing202@hotmail.com>
2017-06-26 20:41:10 +0800

    add welthweather function

--- src/childbreath/service/HealthWeather2.java
+++ src/childbreath/service/HealthWeather2.java
@@ -9,30 +9,139 @@ import java.util.Vector;
 import java.util.HashMap;
 
 public class HealthWeather2 {
-	public HealthWeather2(){
-	}
-	
 	public final static String weather_info_url= "http://222.66.83.21:808/ScreenDisplay/HealthWeather2/webservice/Publish.asmx/GetCrows";
 	public final static String authcode = "authCode=shjkqxxc";
+	public HashMap<String, String> map1;
+	public HashMap<String, String> map2;
 
-	public String getWeatherInfo(){
+	public HealthWeather2(){
 		try {
 			String requestUrl = weather_info_url;
 
 			System.out.println("acess_token_url = " + requestUrl);
 			String w = WeixinUtil.HttpRequestString(requestUrl, "POST", authcode);
-			HashMap<String, String> map1=null;
-			HashMap<String, String> map2=null;
-			parse_weather_string(w, map1, map2);
-			return w;
+			parse_weather_string(w);
 		}catch(Exception e ){
 			System.out.println("jsonobject exception");
 			e.printStackTrace();
-			return null;
 		}
 	}
 
-	public void parse_weather_string(String s, HashMap<String, String> map1, HashMap<String, String> map2) {
+	public String getWarningLevelDay1() {
+		return map1.get("WarningDesc");
+	}
+
+	public String getWarningLevelDay2() {
+		return map1.get("WarningDesc");
+	}
+
+	public String getYear2() {
+		String date = map2.get("Date");
+		String year = "";
+		for(int i = 0; i < date.length(); i++){
+			if ( date.charAt(i) != '-' ) {
+				year += date.charAt(i);
+			} else {
+				break;
+			}
+		}
+		return year;
+	}
+	public String getMonth1() {
+		String date = map1.get("Date");
+		String month = "";
+		int count = 0;
+		for (int i = 0; i < date.length(); i++) {
+			if (date.charAt(i) == '-') {
+				count += 1;
+			} else if (count == 1){
+				month += date.charAt(i);
+			} else if (count == 2) {
+				break;
+			}
+		}
+		return month;
+	}
+	public String getMonth2() {
+		String date = map2.get("Date");
+		String month = "";
+		int count = 0;
+		for (int i = 0; i < date.length(); i++) {
+			if (date.charAt(i) == '-') {
+				count += 1;
+			} else if (count == 1){
+				month += date.charAt(i);
+			} else if (count == 2) {
+				break;
+			}
+		}
+		return month;
+	}
+	public String getDay1() {
+		String date = map1.get("Date");
+		String month = "";
+		int count = 0;
+		for (int i = 0; i < date.length(); i++) {
+			if (date.charAt(i) == '-' || date.charAt(i) == 'T') {
+				count += 1;
+			} else if (count == 2){
+				month += date.charAt(i);
+			} else if (count == 3) {
+				break;
+			}
+		}
+		return month;
+	}
+	public String getDay2() {
+		String date = map2.get("Date");
+		String month = "";
+		int count = 0;
+		for (int i = 0; i < date.length(); i++) {
+			if (date.charAt(i) == '-' || date.charAt(i) == 'T') {
+				count += 1;
+			} else if (count == 2){
+				month += date.charAt(i);
+			} else if (count == 3) {
+				break;
+			}
+		}
+		return month;
+	}
+
+	public String getYear1() {
+		String date = map1.get("Date");
+		String year = "";
+		for(int i = 0; i < date.length(); i++){
+			if ( date.charAt(i) != '-' ) {
+				year += date.charAt(i);
+			} else {
+				break;
+			}
+		}
+		return year;
+	}
+	public String getWarningDesc1(){
+		return map1.get("WarningDesc");
+	}
+	public String getWarningDesc2(){
+		return map2.get("WarningDesc");
+	}
+
+	public String getGuide1(){
+		return map1.get("Wat_guid");
+	}
+	public String getGuide2(){
+		return map2.get("Wat_guid");
+	}
+
+	public String getInflu1(){
+		return map1.get("Influ");
+	}
+	public String getInflu2(){
+		return map2.get("Influ");
+	}
+
+	public void parse_weather_string(String s) {
 		if ( s.length() == 0) return;
 		try {
 			System.out.println("weather_string = "+s);
@@ -56,7 +165,8 @@ public class HealthWeather2 {
 				System.out.println("v"+i+"="+v.elementAt(i));
 				String e = v.elementAt(i);
 				if (e.contains("ID") && e.contains("2")) {
-					map1=m;
+					map1=(HashMap)m.clone();
+					System.out.println("1map1="+map1.toString());
 				} else if (e.contains("Date")) {
 					m.put("Date", ignore_brease(e));
 				} else if (e.contains("WarningLevel")) {
@@ -71,8 +181,6 @@ public class HealthWeather2 {
 			}
 
 			map2=m;
-
-
 			System.out.println("map1="+map1.toString());
 			System.out.println("map2=" + map2.toString());
 			return;
